19e3722b50ff6ebb9b2da85a19654cb12a7a5613
onlytiancai <onlytiancai@gmail.com>
2016-01-30 23:29:40 +0800

    限速，批量，定时刷新

--- src/test/stream-test.js
+++ src/test/stream-test.js
@@ -1,10 +1,13 @@
-/* 演示流的限速, 读取到n个数据，批量写一次 */
-
 var stream = require('stream');
 var util = require('util');
-var byline = require('byline');
 
-function BulkStream(batch, interval){
+/* 
+ * 对输入流进行定时写和批量写, 以及限速，提高写性能
+ *      batch: 收到多少次请求批量写一次, 默认3个一批
+ *      interval: 定时多少毫秒写一次, 默认1秒刷新一次
+ *      maxCount: interval时间内最多读多少次数据, 默认1秒1个
+ * */
+function BulkStream(batch, interval, maxCount){
     stream.Transform.call(this);
 
     this._readableState.objectMode = true;
@@ -12,13 +15,20 @@ function BulkStream(batch, interval){
     this._batch = batch || 3;
     this._cache = [];
     this._interval = interval || 1000;
+    this._maxCount = maxCount || interval / 1000;
     this._runner = null;
-    this._last_cb = null;
-   }
+    this._count = 0;
+}
 
 util.inherits(BulkStream, stream.Transform);
 
 BulkStream.prototype._transform = function(obj, encoding, cb){
+    this._count++;
+    if (this._count > this._maxCount) {
+        this.emit('overload');
+        return cb();
+    }
+
     var that = this;
     obj = obj.toString().trim();
     obj && this._cache.push(obj);
@@ -28,23 +38,22 @@ BulkStream.prototype._transform = function(obj, encoding, cb){
         this._cache = [];
     }
 
-    this._last_cb && this._last_cb();
-    this._last_cb = cb;
-
     if (this._runner == null) {
         this._runner = setTimeout(function(){
             clearTimeout(that._runner);
             that._runner = null;
+
+            that._count = 0;
 
             if (that._cache.length > 0) {
                 that.push(JSON.stringify(that._cache));
                 that._cache = [];
             }
 
-            that._last_cb && that._last_cb();
-            that._last_cb = null;
         }, this._interval);
     }
+
+    cb();
 };
 
 BulkStream.prototype._flush = function(done){
@@ -55,6 +64,11 @@ BulkStream.prototype._flush = function(done){
     done();
 };
 
+var bs = new BulkStream(3, 3000, 3)
+    .on('overload', function(){
+        console.log('overload ...'); 
+    });
+
 process.stdin
-    .pipe(new BulkStream(3, 2000))
+    .pipe(bs)
     .pipe(process.stdout);
