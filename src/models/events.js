afe4d29eedfa8308850e88000cc235a1871cec86
onlytiancai <onlytiancai@gmail.com>
2016-01-31 20:11:02 +0800

    重构

--- src/models/events.js
+++ src/models/events.js
@@ -15,7 +15,6 @@ var db = require('./mysql_db');
 /*
 * 跟踪一个事件
 */
-
 function track(options, properties, callback) {
     if (! (options.app_id && options.user_id && options.event_name)) {
         throw Error("app_id, user_id, event_name all required."); 
@@ -41,7 +40,7 @@ function track(options, properties, callback) {
 }
 
 // TODO: 有多余计算
-function calcData(metrics, groupOn, type){
+function calcGroup(metrics, groupOn, type){
     return function(list, key) {
         var count = 0, sum = 0, avg = 0, min = 0,
             max = 0, found = null; 
@@ -69,13 +68,66 @@ function getPropertiesByRow(row) {
     return properties;
 }
 
-function getData(metrics, groupOn, strWhere, type){
-    return function(rows, memo){
-         return _.chain(rows).map(getPropertiesByRow)
-        .filter(jsonWhere(strWhere))
-        .groupBy(groupOn) 
-        .map(calcData(memo, metrics, groupOn, type))
-        .value();
+function processResult(x){
+    delete x.count_;
+    delete x.sum_;
+    delete x.avg_;
+    delete x.max_;
+    delete x.min_;
+    return x;
+}
+
+function mergeData(data, newData, groupOn, metrics, type) {
+    _.each(newData, function(item){
+        var found = _.filter(data, function(x) {
+            return x[groupOn] == item[groupOn];
+        });
+
+        if (found.length > 0) {
+            found[0].count_ += item.count_;
+            found[0].sum_ += item.sum_;
+            found[0].avg_ = found[0].sum_ / found[0].count_;
+            found[0].max_ = _.max([item.max_, found[0].max_]);
+            found[0].min_ = _.min([item.min_, found[0].min_]);
+            found[0][metrics] = found[0][type + '_'];
+
+        }else {
+            data.push(item);
+        }
+    });
+
+    return data;
+}
+
+function getRows(app_id, user_id, event_name, from_date, to_date) {
+    var page = 0, pageSize = 2;
+
+    return function(callback) {
+        var offset = page * pageSize;
+        page = page + 1;
+
+        db.fetch_events( offset, pageSize, app_id, user_id, event_name,
+            from_date, to_date, callback); 
+    };
+}
+
+
+function calcData(getData, strWhere, groupOn, metrics, type, callback) {
+    return function(callback){
+        getData(function(err, rows){
+            if (err) return callback(err, null);
+
+            var ret = {rowLength: rows.length, memo: []};
+            if(rows.length === 0) return callback(null, ret);
+
+            ret.memo = _.chain(rows).map(getPropertiesByRow)
+            .filter(jsonWhere(strWhere))
+            .groupBy(groupOn) 
+            .map(calcGroup(metrics, groupOn, type))
+            .value();
+
+            callback(null, ret);
+        });
     };
 }
 
@@ -113,77 +165,16 @@ function query(options, strWhere, callback) {
 
     // TODO; groupOn err
     
-    var page = 0, pageSize = 2;
-    var getRows = function(callback) {
-        var offset = page * pageSize;
-        page = page + 1;
-
-        db.fetch_events(
-            offset,
-            pageSize,
-            app_id,
-            user_id,
-            event_name,
-            from_date, 
-            to_date,
-            callback); 
-    };
-
-    var calcMemo = function(callback) {
-        getRows(function(err, rows){
-            if (err) return callback(err, null);
-
-            var ret = {rowLength: rows.length, memo: []};
-            if(rows.length === 0) return callback(null, ret);
-
-            ret.memo = _.chain(rows).map(getPropertiesByRow)
-            .filter(jsonWhere(strWhere))
-            .groupBy(groupOn) 
-            .map(calcData(metrics, groupOn, type))
-            .value();
-
-            callback(null, ret);
-        });
-    }
-    
-    var processResult = function(x){
-        delete x.count_;
-        delete x.sum_;
-        delete x.avg_;
-        delete x.max_;
-        delete x.min_;
-        return x;
-    };
-
-    var mergeData = function(data, newData, groupOn, metrics) {
-        _.each(newData, function(item){
-            var found = _.filter(data, function(x) {
-                return x[groupOn] == item[groupOn];
-            });
-
-            if (found.length > 0) {
-                found[0].count_ += item.count_;
-                found[0].sum_ += item.sum_;
-                found[0].avg_ = found[0].sum_ / found[0].count_;
-                found[0].max_ = _.max([item.max_, found[0].max_]);
-                found[0].min_ = _.min([item.min_, found[0].min_]);
-                found[0][metrics] = found[0][type + '_'];
-
-            }else {
-                data.push(item);
-            }
-        });
-        
-        return data;
-    };
-
+    var fnGetRows = getRows(app_id, user_id, event_name, from_date, to_date); 
+    var fnCalcData = calcData(fnGetRows, strWhere, groupOn, metrics, type);
     var stop = false, data = []; //必须外部变量hold住回调结果，否则就undefined了
+
     async.whilst(
         function() { return !stop; },
         function(callback) {
-            calcMemo(function(err, ret){
+            fnCalcData(function(err, ret) {
                 stop = ret.rowLength == 0;
-                data = mergeData(data, ret.memo, groupOn, metrics);
+                data = mergeData(data, ret.memo, groupOn, metrics, type);
                 callback(err, data); 
             });
         },
